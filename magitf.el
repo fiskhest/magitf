7fe3069723ca75c790e5646ee7c3cfe4deeb0cb8
Johan Radivoj <johan.radivoj@zimpler.com>
2023-06-19 00:08:38 +0200

    Cleanup, make emacs generic, conform to package-lint

--- magitf.el
+++ magitf.el
@@ -1,27 +1,37 @@
-;;; config.el -                                    -*- lexical-binding: t; -*-
-
+;;; magitf.el --- A terraform porcelain -*- lexical-binding: t; -*-
+;;
 ;; Copyright (C) 2023  Johan Radivoj
+;;
+;; Author: Johan Radivoj <https://github.com/fiskhest>
+;; Maintainer: Johan Radivoj
+;; Version: 1.0
+;; Homepage: https://github.com/fiskhest/magitf.el
+;; Package-Requires: ((emacs "28.1"))
 
-;; Author: Johan Radivoj <johan@radivoj.se>
-;; Keywords: lisp,
+;; This file is NOT part of GNU Emacs.
 
-(defvar +terraform-runner (if (executable-find "terragrunt") "terragrunt" "terraform")
-  "The default runner - terraform or terragrunt")
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-(when (modulep! :tools magitf)
- (map! :leader
-   (:prefix-map ("g" . "git")
-          :desc "magitf status"    "T"   #'magitf-status))
- (map! :map terraform-mode-map
-       :localleader
-       :desc "terraform" "t" #'magitf-status))
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-(when (modulep! +lsp)
-  (add-hook 'terraform-mode-local-vars-hook #'lsp! 'append))
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; magitf.el provides a magit-like interactive experience for terraform in Emacs.
+
+;;; Code:
+
+;;;; Requirements
+(require 'transient)
 (eval-when-compile (require 'subr-x))
-
-(use-package transient-showcase)
 
 (transient-define-prefix magitf-infix-init ()
 
@@ -82,9 +92,6 @@
 (transient-define-prefix magitf-status ()
   "magitf - porcelain for temacsrraform"
 
-  ; only one kind of eyes is meaningful at a time
-  ; :incompatible '(("-b" "-g" "-p" "-s" "-t" "-w" "-y"))
-
    ["Terraform"
     ["Actions"
      ; ("p" "plan" magitf--suffix-plan :transient transient--do-call)
@@ -100,10 +107,9 @@
      ("s" "state" magitf-infix-state)
      ("u" "force-unlock" magitf-suffix-force-unlock)]
      ; ("m" "import" magitf-suffix-placeholder)]  ; not yet implemented
-    ["\n"]
     ["Controls"
      ("b" "delete buffer" magitf--quit-buffer)
-     ("q" "quit" evil-escape)]
+     ("q" "quit" keyboard-escape-quit)]
    ])
 
 (transient-define-suffix magitf--quit-buffer ()
@@ -115,95 +121,33 @@
 (defun magitf--buffer-exists-p ()
   "Visibility predicate."
   (not (equal (get-buffer "*magitf*") nil)))
-
-; found this from some working example but didn't get it working here
-;; (transient-define-suffix magitf--clear-buffer (&optional buffer)
-;;   "Delete the *magitf* buffer.  Optional BUFFER name."
-;;   :transient 'transient--do-call
-;;   :if 'magitf--buffer-exists-p
-;;   (interactive) ; todo look at "b" interactive code
-
-;;   (save-excursion
-;;     (let ((buffer (or buffer "*magitf*")))
-;;       (set-buffer buffer)
-;;       (delete-region 1 (+ 1 (buffer-size))))))
-
-;; (defun execute-commands (buffer &rest commands)
-;;   "Execute a list of shell commands sequentially"
-;;   (with-current-buffer buffer
-;;     (set (make-local-variable 'commands-list) commands)
-;;     (start-next-command)))
-
-;; (defun start-next-command ()
-;;   "Run the first command in the list"
-;;   (if (null commands-list)
-;;       (insert "\nDone.")
-;;     (let ((command  (car commands-list)))
-;;       (setq commands-list (cdr commands-list))
-;;       (insert (format ">>> %s\n" command))
-;;       (let ((process (start-process-shell-command command (current-buffer) command)))
-;;         (set-process-sentinel process 'sentinel)))))
-
-;; (defun sentinel (p e)
-;;   "After a process exited, call `start-next-command' again"
-;;   (let ((buffer (process-buffer p)))
-;;     (when (not (null buffer))
-;;       (with-current-buffer buffer
-;;         ;(insert (format "Command `%s' %s" p e) )
-;;         (start-next-command)))))
-
-  ; (ansi-color-apply-on-region (window-start) (window-end) t)
-    ;; (when (magitf--buffer-exists-p)
-    ;;   (magitf--clear-buffer))
-    ; (start-process "terraform" "*magitf*" "terraform" "plan")
-    ;; (cmd! compile (format "%s plan" +terraform-runner)))
-    ; (switch-to-buffer "*magitf*"))
-
 (transient-define-suffix magitf-suffix-init(&optional args)
   (interactive (list (transient-args transient-current-command)))
   (magitf--cmd-to-buffer "terraform init" args)
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "init" args)
   (magitf--set-buffer-read-only)
-  (evil-escape)
-  )
+  (keyboard-escape-quit))
 
 (transient-define-suffix magitf-suffix-validate (&optional args)
   (interactive (list (transient-args transient-current-command)))
   (magitf--cmd-to-buffer "terraform validate" args)
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "validate" args)
   (magitf--set-buffer-read-only)
-  (evil-escape)
-  )
+  (keyboard-escape-quit))
 
 (transient-define-suffix magitf-suffix-fmt(&optional args)
   (interactive (list (transient-args transient-current-command)))
   (magitf--cmd-to-buffer "terraform fmt" args)
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "fmt" args)
   (magitf--set-buffer-read-only)
-  (evil-escape)
-  )
-
-;; this is a working example - dont mess it up
-;; (transient-define-suffix magitf-suffix-plan (&optional args)
-;;   (interactive (list (transient-args transient-current-command)))
-;;   (with-current-buffer (get-buffer-create "*magitf*")
-;;    (let ((inhibit-read-only t)) (set-text-properties (point-min) (point-max) ()))
-;;    (read-only-mode -1) (erase-buffer))
-;;   (with-current-buffer "*magitf*"
-;;     ;(insert (propertize (format ">>> terraform plan %s\n" (or (string-join args "") "")) 'read-only t)))
-;;     (insert (format ">>> terraform plan %s\n" (or (string-join args "") "")) ))
-;;   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "plan" args)
-;;   (with-current-buffer "*magitf*" (read-only-mode 1))
-;;   (switch-to-buffer "*magitf*")
-;;   (evil-escape))
+  (keyboard-escape-quit))
 
 (transient-define-suffix magitf-suffix-plan (&optional args)
   (interactive (list (transient-args transient-current-command)))
   (magitf--cmd-to-buffer "terraform plan" args)
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "plan" args)
   (magitf--set-buffer-read-only)
-  (evil-escape)
-  )
+  (keyboard-escape-quit))
 
 (transient-define-suffix magitf-suffix-apply (&optional args)
   (interactive (list (transient-args transient-current-command)))
@@ -211,8 +155,7 @@
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "apply" args)
   ; Should not be read only because eventually tf prompts requiring input.
   ; Can we instead use read-y-or-n and input into the tf prompt? (for y/n instead of arbitrary anything allowed to be input atm)
-  (magitf--set-buffer-write)
-  )
+  (magitf--set-buffer-write))
 
 (transient-define-suffix magitf-suffix-destroy (&optional args)
   (interactive (list (transient-args transient-current-command)))
@@ -220,19 +163,18 @@
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "destroy" args)
   ; Should not be read only because eventually tf prompts requiring input.
   ; Can we instead use read-y-or-n and input into the tf prompt? (for y/n instead of arbitrary anything allowed to be input atm)
-  (magitf--set-buffer-write)
-  )
+  (magitf--set-buffer-write))
 
 (transient-define-suffix magitf-suffix-console (&optional args)
   (interactive (list (transient-args transient-current-command)))
   (magitf--cmd-to-buffer "terraform console" args)
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil "console" args)
   ; Should not be read only because this is sorta a REPL which we want to continue interaction with
-  (magitf--set-buffer-write)
-  )
+  (magitf--set-buffer-write))
 
 (transient-define-suffix magitf-suffix-force-unlock (id)
-  ; TODO: find a way to require input. Or use completing-read and populate with some auto-fetched state->id mapping for better UX
+  ; TODO: find a way to require input (currently RET ing empty input becomes `tf force-unlock ""'
+  ; Or use completing-read and pre-populate with some auto-fetched state->id mapping for better UX
   (interactive "sID?: ")
   (magitf--cmd-to-buffer "terraform force-unlock" (list id))
   (apply #'make-comint-in-buffer "magitf" nil "terraform" nil (list "force-unlock" id))
@@ -240,8 +182,7 @@
   ; Should not be read only because eventually tf prompts requiring input.
   ; Can we instead use read-y-or-n and input into the tf prompt? (for y/n instead of arbitrary anything allowed to be input atm)
   ; (with-current-buffer "*magitf*" (read-only-mode 1))
-  (magitf--set-buffer-write)
-  )
+  (magitf--set-buffer-write))
 
 (defun magitf--set-buffer-write ()
   (with-current-buffer "*magitf*" (read-only-mode -1)))
@@ -252,8 +193,7 @@
 (defun magitf--reset-buffer ()
   (with-current-buffer (get-buffer-create "*magitf*")
    (let ((inhibit-read-only t)) (set-text-properties (point-min) (point-max) ()))
-   (read-only-mode -1) (erase-buffer)
-  ))
+   (read-only-mode -1) (erase-buffer)))
 
 (defun magitf--cmd-to-buffer (cmd-input &optional args)
   (magitf--reset-buffer)
@@ -265,20 +205,12 @@
   ; we want to combine args and id into one apply in this func but I'm not getting it to work...
   ; (apply #'make-comint-in-buffer "magitf" nil cmd nil (list subcmd (or args "")))
   (magitf--set-buffer-write)
-  (switch-to-buffer "*magitf*")
-  )
+  (switch-to-buffer "*magitf*"))
 
 (defun magitf-suffix-placeholder ()
   "Wave at the user."
   (interactive)
   (message "Waves at the user at: %s." (current-time-string)))
 
-;; clever code I've found to maybe be reused
-;; (when (magitf--buffer-exists-p)
-;;   (magitf--clear-buffer))
-;; (set-process-sentinel (start-process "terraform" buffer ("terraform" "plan" args)) #'output-message-sentinel)
-
-;; (defun output-message-sentinel (process msg)
-;;   (when (memq (process-status process) '(exit signal))
-;;     (message (concat (process-name process) " - " msg))))
-; (apply #'cmd! (compile (format "terraform plan %s" args)))))
+(provide 'magitf)
+;;; magitf.el ends here
